1ad6c5e2b37e2e7cec40208df515309f0f72921a
Julian Bogdani <jbogdani@gmail.com>
2025-11-18 16:48:09 +0100

    refactor: improve Navbar component structure and formatting for better readability

--- src/usr/modules/NavBar.js
+++ src/usr/modules/NavBar.js
@@ -7,10 +7,9 @@ const StickyMenu = styled.div`
   position: sticky;
   top: 0;
   z-index: 100;
-`;
+`
 
 function FN_Navbar() {
-  
   return (
     <StickyMenu>
       <Menu>
@@ -18,45 +17,71 @@ function FN_Navbar() {
           <Container>
             <Navbar.Brand href={withPrefix(`/`)}>FortNet Project</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-            <Nav>
-              <NavDropdown title="About" id="nav-dropdown">
-                <NavDropdown.Item href={withPrefix('/objectives/')}>Objectives</NavDropdown.Item>
-                <NavDropdown.Item href={withPrefix('/methodology/')}>Methodology</NavDropdown.Item>
-                <NavDropdown.Item href={withPrefix('/deliverables/')}>
-                  Deliverables
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href={withPrefix('/people/')}>Peolpe</NavDropdown.Item>
-              </NavDropdown>
-              
-              <NavDropdown title="Activities" id="nav-dropdown">
-                <NavDropdown.Item href={withPrefix('/field-research/')}>Field research</NavDropdown.Item>
-                <NavDropdown.Item href={withPrefix('/team-meetings/')}>Team meetings</NavDropdown.Item>
-                <NavDropdown.Item href={withPrefix('/workshop-and-conferences/')}>Workshop and conferences</NavDropdown.Item>
-                <NavDropdown.Item href={withPrefix('/lectures-and-presentations/')}>Lectures and presentations</NavDropdown.Item>
-                <NavDropdown.Item href={withPrefix('/public-engagement/')}>Public engagement</NavDropdown.Item>
-              </NavDropdown>
-              
-              <Nav.Item>
-                <Nav.Link title="Fortnet Atlas" href={withPrefix('/atlas/')}>
-                  Fortnet Atlas
-                </Nav.Link>
-              </Nav.Item>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
+                <NavDropdown title="About" id="nav-dropdown">
+                  <NavDropdown.Item href={withPrefix("/objectives/")}>
+                    Objectives
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href={withPrefix("/methodology/")}>
+                    Methodology
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href={withPrefix("/deliverables/")}>
+                    Deliverables
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href={withPrefix("/people/")}>
+                    Peolpe
+                  </NavDropdown.Item>
+                </NavDropdown>
 
-              <Nav.Item>
-                <Nav.Link title="Fortnet Atlas" href={withPrefix('/bibliography-geo-viewer/')}>
-                  Bibliography Geo-viewer
-                </Nav.Link>
-              </Nav.Item>
-              
-              <Nav.Item>
-                <Nav.Link title="Fortnet Atlas" href={withPrefix('/contacts/')}>
-                  Contacts
-                </Nav.Link>
-              </Nav.Item>
+                <NavDropdown title="Activities" id="nav-dropdown">
+                  <NavDropdown.Item href={withPrefix("/field-research/")}>
+                    Field research
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href={withPrefix("/team-meetings/")}>
+                    Team meetings
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    href={withPrefix("/workshop-and-conferences/")}
+                  >
+                    Workshop and conferences
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    href={withPrefix("/lectures-and-presentations/")}
+                  >
+                    Lectures and presentations
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href={withPrefix("/public-engagement/")}>
+                    Public engagement
+                  </NavDropdown.Item>
+                </NavDropdown>
 
-            </Nav>
+                <Nav.Item>
+                  <Nav.Link title="Fortnet Atlas" href={withPrefix("/atlas/")}>
+                    Fortnet Atlas
+                  </Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link
+                    title="Fortnet Atlas"
+                    href={withPrefix("/bibliography-geo-viewer/")}
+                  >
+                    Bibliography Geo-viewer
+                  </Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                  <Nav.Link
+                    title="Fortnet Atlas"
+                    href={withPrefix("/contacts/")}
+                  >
+                    Contacts
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
       </Menu>
